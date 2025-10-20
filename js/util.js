fc8fdf7cbe8b9a0908a3cc83411c0722014da0ec
Павел <pasaakovlev720@gmail.com>
2025-10-20 20:50:15 +0500

    Разделил код на модули: util.js, data.js, main.js

---
+++ js/util.js
@@ -0,0 +1,9 @@
+export const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+export const createIdGenerator = () => {
+  let lastId = 0;
+  return () => {
+    lastId += 1;
+    return lastId;
+  };
+};
